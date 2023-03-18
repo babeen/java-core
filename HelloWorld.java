b14d8579b7f2c2a48d0933cfe414604bc2ce3f82
babeen <babinnepalibtl@gmail.com>
2023-03-18 20:41:54 +0545

    hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 	public static void main(String[]args){
 		for (int i = 0; i < 1000; i++) {
 			
-			System.out.println("I am sorry, Astha");
+			System.out.println("I am sorry!");
 		}
 	}
 }
